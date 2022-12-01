3971da5a2deea96d6649f62f53f74aea86ec4e8e
kannotyan <18097@hakodate.kosen-ac.jp>
2022-12-01 09:56:43 +0900

    mariko 上下左右

--- game3/Mariko.java
+++ game3/Mariko.java
@@ -15,5 +15,21 @@ public class Mariko extends Actor
     public void act()
     {
         // Add your action code here.
+        if( Greenfoot.isKeyDown( "up" ) ){
+            setRotation(-90);
+            move(1);
+        }
+        if( Greenfoot.isKeyDown( "down" ) ){
+            setRotation(90);
+            move(1);
+        }
+        if( Greenfoot.isKeyDown( "left" ) ){
+            setRotation(180);
+            move(1);
+        }
+        if( Greenfoot.isKeyDown( "right" ) ){
+            setRotation(0);
+            move(1);
+        }
     }
 }
