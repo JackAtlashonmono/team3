f1a6d64394a4c9c10d31c2a2803afde64d64ba20
yuhiii <18093@hakodate.kosen-ac.jp>
2022-12-15 09:56:34 +0900

    a

--- game3/Mariko.java
+++ game3/Mariko.java
@@ -12,24 +12,26 @@ public class Mariko extends Actor
      * Act - do whatever the Mariko wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private int gravity;
     public void act()
     {
+         gravity--;
+         setLocation(getX(), getY() - gravity);
+         checkForJump();
         // Add your action code here.
-        if( Greenfoot.isKeyDown( "up" ) ){
-            setRotation(-90);
-            move(1);
+        if( Greenfoot.isKeyDown( "A" ) ){
+            setRotation(0);
+            move(-1);
         }
-        if( Greenfoot.isKeyDown( "down" ) ){
-            setRotation(90);
-            move(1);
-        }
-        if( Greenfoot.isKeyDown( "left" ) ){
-            setRotation(180);
-            move(1);
-        }
-        if( Greenfoot.isKeyDown( "right" ) ){
+        if( Greenfoot.isKeyDown( "D" ) ){
             setRotation(0);
             move(1);
         }
+       
+    }
+    private void checkForJump()
+    {
+            if( Greenfoot.isKeyDown("space"))
+                gravity = 20; // this will make the character jump
     }
 }
